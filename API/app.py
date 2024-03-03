db61a3ef70ed1cf374897d55fe8aaaf45643c8e5
JustTheCoolest <balu.somenumbers@gmail.com>
2024-03-03 08:28:50 +0530

    get method structure created

--- API/app.py
+++ API/app.py
@@ -42,14 +42,29 @@ class Analyser(Resource):
     parser.add_argument('capital_gains_tax_ratio', type=float)
     parser.add_argument('target_profit_ratio', type=float)
 
-    def get(self, stock_name):
-        return {'hello': 'world'}
-    
+    @staticmethod
+    def target_profits(price_at_buy, fee_ratio_at_buy, fee_ratio_at_sell, capital_gains_tax_ratio, target_profit_ratio):
+        def _intermediate_aggregator():
+            pass
+        pass
+
+    def get(self, stock_name = None):
+        if stock_name == None:
+            cursor.execute('SELECT name FROM Stocks')
+            return {'Available Stocks': cursor.fetchall()}
+        cursor.execute(f"SELECT * FROM Stocks WHERE name = '{stock_name}'")
+        stock_data = cursor.fetchone()
+        if not stock_data:
+            return {'message': 'Stock not found'}, 404
+        # price_to_sell_with_strict_target, price_to_sell_with_year_target = 
+        return Analyser.target_profits(None, None, None, None, None)
+
+
     def post(self, stock_name):
         return {'hello': 'world'}
-    
-api.add_resource(Analyser, '/<string:stock_name>')
+
+api.add_resource(Analyser, '/', '/<string:stock_name>')
 
 if __name__ == "__main__":
     table_setup()
-    app.run()
+    app.run(debug=True)
